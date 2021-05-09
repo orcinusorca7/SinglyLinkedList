0985a8ea20fd02e40237c2f5ab5b8eb094d3776c
ChandimaHerath <chandimaherath@hotmail.com>
2021-05-09 23:18:20 +0530

    main method implemented

--- src/LinkedListDemo.java
+++ src/LinkedListDemo.java
@@ -1,2 +1,14 @@
 public class LinkedListDemo {
-}
+    public static void main(String[] args) {
+
+            LinkedList List = new LinkedList();
+            List.addNode(1);
+            List.addNode(2);
+            List.addNode(3);
+            List.addNode(4);
+
+            List.display();
+        }
+
+    }
+

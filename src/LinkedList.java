5b91f1cd579b0088fc02c91de40cd59d78882429
ChandimaHerath <chandimaherath@hotmail.com>
2021-05-09 19:51:10 +0530

    node adding method implemented

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,2 +1,26 @@
 public class LinkedList {
+    class Node{
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public Node head = null;
+    public Node tail = null;
+
+    public void addNode(int data) {
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
 }
